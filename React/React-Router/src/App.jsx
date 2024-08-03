3bcf911bc8c8df97d87d7b6420d3a3630dd762d9
ankur-upflairs <ankur.upflairs@gmail.com>
2024-08-03 10:41:19 +0530

    state

--- React/React-Router/src/App.jsx
+++ React/React-Router/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blogs from './pages/Blogs'
 import CreateBlog from './pages/CreateBlog'
+import Events from './pages/Events'
 
 function App() {
  
@@ -17,12 +18,15 @@ function App() {
         <li><Link to={''} >Home</Link></li>
         <li><Link to={'blogs'}>blogs</Link></li>
         <li><Link to={'createblog'}>create blog</Link></li>
+        <li><Link to={'event'}>Events</Link></li>
+
       </ul>       
       
       <Routes>
         <Route path='' element={<Home/>}/>
         <Route path='blogs' element={<Blogs/>}/>
         <Route path='createblog' element={<CreateBlog/>}/>
+        <Route path='event' element={<Events/>}/>
       </Routes>      
       </BrowserRouter>
     </>
